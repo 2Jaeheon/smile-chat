7a22c1027766cf46ee13f9f634f2a156d8335d71
jaeheon0826 <jaeheon0826@jbnu.ac.kr>
2024-12-13 16:46:20 +0900

    feat: cognito login and register setting

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {AuthProvider} from "react-oidc-context";
+
+const cognitoAuthConfig = {
+    authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_GiMEcaGt4",
+    client_id: "4i5aodon6970pe8e51n9ptokde",
+    redirect_uri: "https://main.d1iozj3igvpaxt.amplifyapp.com",
+    response_type: "code",
+    scope: "phone openid email",
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    document.getElementById('root') as HTMLElement
 );
 
+// wrap the application with AuthProvider
+root.render(
+    <React.StrictMode>
+        <AuthProvider {...cognitoAuthConfig}>
+            <App/>
+        </AuthProvider>
+    </React.StrictMode>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
