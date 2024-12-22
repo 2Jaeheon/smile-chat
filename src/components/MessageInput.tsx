690a574c8ef74302638a02bbfeb44cab025d72f2
jaeheon0826 <jaeheon0826@jbnu.ac.kr>
2024-12-22 16:04:32 +0900

    feat: message Input

--- src/components/MessageInput.tsx
+++ src/components/MessageInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/MessageInput.css';
+import '../styles/MessageInput.css'; // 스타일 파일을 불러옵니다.
 
 interface MessageInputProps {
     newMessage: string;
@@ -9,14 +9,16 @@ interface MessageInputProps {
 
 const MessageInput: React.FC<MessageInputProps> = ({newMessage, setNewMessage, handleSendMessage}) => {
     return (
-        <div className="message-input">
-            <input
-                type="text"
-                value={newMessage}
-                onChange={(e) => setNewMessage(e.target.value)}
-                placeholder="Type your message..."
-            />
-            <button onClick={handleSendMessage}>Send</button>
+        <div className="message-input-container">
+            <div className="message-input">
+                <input
+                    type="text"
+                    value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    placeholder="Type your message..."
+                />
+                <button onClick={handleSendMessage}>Send</button>
+            </div>
         </div>
     );
 };
