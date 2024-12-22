1cac850c385b9cacaf0b83b31fc167af0d9c3b0e
jaeheon0826 <jaeheon0826@jbnu.ac.kr>
2024-12-23 01:41:37 +0900

    feat: input maximum charaters

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import MessageCard from './MessageCard';
 import '../styles/MessageList.css';
 
@@ -14,33 +14,18 @@ interface MessageListProps {
 }
 
 const MessageList: React.FC<MessageListProps> = ({messages}) => {
-    const [itemsToShow, setItemsToShow] = useState(6);
+    const messageListRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth < 768) {
-                setItemsToShow(5); // 모바일 화면에서는 5개
-            } else {
-                setItemsToShow(6); // PC 화면에서는 6개
-            }
-        };
-
-        // 처음 렌더링 시와 창 크기 변경 시 이벤트 처리
-        handleResize();
-        window.addEventListener('resize', handleResize);
-
-        // 컴포넌트가 언마운트 될 때 이벤트 리스너 제거
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    // 최근 6개의 메시지 또는 화면 크기에 맞게 메시지 수 설정
-    const recentMessages = messages.slice(-itemsToShow);
+        if (messageListRef.current) {
+            // 페이지 로딩 후 맨 아래로 스크롤
+            messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+        }
+    }, [messages]);
 
     return (
-        <div className="message-list">
-            {recentMessages.map((message) => (
+        <div className="message-list" ref={messageListRef}>
+            {messages.map((message) => (
                 <MessageCard key={message.messageId} message={message}/>
             ))}
         </div>
